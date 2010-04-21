0cb573091ebad853df69518255ab067429b3f78c
Jack <jack.polgar@gmail.com>
2010-04-21 11:51:32 +1000

    First commit

---
+++ fishhook.class.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * FishHook 3.0
+ * Copyright (C) 2010 Jack Polgar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 only.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class FishHook
+{
+	/**
+	 * Hook
+	 * Used to fetch plugin code for the specified hook.
+	 */
+	public static function hook($hook)
+	{
+		// Fetch the plugin code from the DB.
+		$code = array();
+		$fetch = mysq_query("SELECT * FROM plugin_code WHERE hook='".mysql_real_escape_string($hook)."' AND enabled='1' ORDER BY execorder ASC");
+		while($info = mysql_fetch_array($fetch))
+			$code[] = ($info['code']);
+		
+		return implode(" /* */ ",$code);
+	}
+}
+?>
